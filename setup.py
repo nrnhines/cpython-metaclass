5897250200e1cf16910e9543bb8fccd3e9d5251c
Michael Hines <michael.hines@yale.edu>
2024-08-15 07:33:01 -0400

    compile with -O0

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 from distutils.core import setup, Extension
 
-module1 = Extension('demo',
-                    sources = ['demo.cpp'])
+module1 = Extension("demo", sources=["demo.cpp"], extra_compile_args=["-O0"])
 
-setup (name = 'PackageName',
-       version = '1.0',
-       description = 'This is a demo package',
-       ext_modules = [module1])
+setup(
+    name="PackageName",
+    version="1.0",
+    description="This is a demo package",
+    ext_modules=[module1],
+)
